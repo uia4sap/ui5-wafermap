0bae9bd52c9917b9146d73aadce10e6d67afd22a
Kyle K. Lin <gazer.kanlin@gmail.com>
2022-07-20 09:26:20 +0800

    Update 20220720

--- src/ui5/wafermap/3rdparty/uia-wafermap/uia-wafermap.js
+++ src/ui5/wafermap/3rdparty/uia-wafermap/uia-wafermap.js
@@ -284,11 +284,13 @@
               die.type = die.type + "Good";
             }
             die.pass = true;
+            die.data = null;
           } else {
             if (die.pass) {
               die.type = "GoodBad";
             }
             die.pass = false;
+            die.data = _layer.data(rowOffset, colOffset);
           }
         }
         if (die.code >= 0) {
